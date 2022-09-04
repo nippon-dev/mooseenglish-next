ca8f257affa9ae162b00fd2dddbc2894332b1ea5
Kevin Miller Jr <kevinmillerjr@gmail.com>
2022-09-04 10:29:40 +0700

    Added bottom border to navbar

--- pages/index.js
+++ pages/index.js
@@ -136,7 +136,7 @@ function classNames(...classes) {
 export default function LandingPage() {
   return (
     <div className="bg-yellow-900">
-      <header className="top-0 sticky z-50">
+      <header className="top-0 sticky z-50 border-b-2 border-moosegreen">
         <Popover className="relative bg-moosepink">
           <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
             <div className="flex justify-start lg:w-0 lg:flex-1">
